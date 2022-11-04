dce00e1548b19c99cc5ca1ade66c0289a6dff63b
Bruberlul <116936826+Bruberlul@users.noreply.github.com>
2022-11-04 00:49:31 -0400

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+		colors: {
+			primary: '#4C9A2A'
+		}
+	},
+	fontFamily: {
+		sans: ['Montserrat', 'sans-serif'],
+	}
+  },
+  plugins: [],
+}
